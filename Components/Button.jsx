3f35f139d908f2c3a22c0854329c487358ae10ab
DannyClark90 <dannyclark_90@hotmail.co.uk>
2024-07-23 14:44:28 +0100

    Burger menu icon added.

--- Components/Button.jsx
+++ Components/Button.jsx
@@ -3,7 +3,7 @@ import styles from "../css/button.module.css"
 export default function Button({text}) {
   return (
     <button className={styles.btn}>
-        <h2>{text}</h2>
+        <h3>{text}</h3>
     </button>
   )
 }
---
+++ Components/Navbar.jsx
@@ -0,0 +1,71 @@
+"use client"
+import styles from "@/css/navbar.module.css"
+import logo from "@/public/Assets/BP_cafe_logo.svg"
+import Image from "next/image"
+import Link from "next/link"
+import { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+
+export default function Navbar() {
+    const [isOpen, setIsOpen] = useState(false)
+
+    const toggleNav = (event) => {
+        event.preventDefault()
+        setIsOpen(!isOpen);
+        console.log(isOpen);
+        }
+
+    const navLinks = [
+        {
+          id: 1,
+          linkName: "Home",
+          link: "/home",
+        },
+        {
+          id: 2,
+          linkName: "Menus",
+          link: "/menus",
+        },
+        {
+          id: 3,
+          linkName: "Private Hire",
+          link: "/private-hire",
+        },
+        {
+          id: 4,
+          linkName: "What's On",
+          link: "/whats-on",
+        },
+        {
+          id: 5,
+          linkName: "Book",
+          link: "/book",
+        },
+        {
+          id: 6,
+          linkName: "Gift Vouchers",
+          link: "https://squareup.com/gift/ML69QV5K93ST6/order",
+        },
+        {
+            id: 7,
+            linkName: "Contact & Hours",
+            link: "/contact",
+          }
+      ];
+  return (
+    <nav className={styles["navbar"]}>
+        <div className={styles["navbar__logo"]}>
+        <Image
+            alt="Gold Black Pig Cafe Logo"
+            src={logo}
+            fill
+        />
+        </div>
+            <ul className={styles["navbar__links"]}>
+                {navLinks.map((link) => {return <li><Link href={link.link}><h3 className={styles["navbar__link"]}>{link.linkName}</h3></Link></li>})}
+            </ul>
+        
+        <FaBars className={styles["navbar__burger-menu"]} onClick={toggleNav}/>
+    </nav>
+  )
+}
