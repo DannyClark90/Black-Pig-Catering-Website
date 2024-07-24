ae8bad67e30a54cb3063783849395d0addf7a75c
DannyClark90 <dannyclark_90@hotmail.co.uk>
2024-07-24 08:41:25 +0100

    Font issue resolved.

--- app/(cafe)/layout.js
+++ app/(cafe)/layout.js
@@ -1,9 +1,13 @@
+import { Cormorant_Garamond, Montserrat } from "next/font/google";
 import "../globals.css";
 import Navbar from "@/Components/Navbar/Navbar";
 
+export const cormorantGaramond = Cormorant_Garamond({ subsets: ["latin"], variable: '--font-cormarant-garamond', weight: '600'});
+export const montserrat = Montserrat({ subsets: ["latin"], variable: '--font-montserrat', weight: '400' });
+
 export default function NavLayout({ children }) {
   return (
-      <body>
+      <body className={`${cormorantGaramond.variable} ${montserrat.variable}`}>
         <Navbar/>
         {children}
       </body>
